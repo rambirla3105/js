f64a1dcd0e045b3adb102e89a23b8608cf4a35f1
Ram Birla <personal310504@gmail.com>
2026-02-07 17:25:51 +0530

    Day 4

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let form = document.querySelector("form");
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    let input = document.querySelector("input");
-    console.log(input.innerText);
+    let user = document.querySelector("#user");
+    let pass = document.querySelector("#pass");
+    alert("Logged In")
+    console.log(`usename ${user.value} password: ${pass.value}`);
 });
