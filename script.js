f889814985274da6187958cf71168dce20937df7
Ram Birla <personal310504@gmail.com>
2026-02-08 22:18:39 +0530

    commit 5

--- script.js
+++
@@ -1,8 +0,0 @@
-let p = document.querySelector("p");
-let input = document.querySelector("input");
-let button = document.querySelector("button");
-
-input.addEventListener("input", function(){
-  console.log(input.value);
-  p.innerText = input.value;
-});
